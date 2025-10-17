679dcf80478c0672177eb78b668c9fe474cbf860
notnat1 <natteishere11@gmail.com>
2025-10-17 13:39:01 +0700

    bootstrap

---
+++ bootstrap/app.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        // Tambahkan TrustProxies
+        $middleware->append(\App\Http\Middleware\TrustProxies::class);
+
+        // Tambahkan trustProxies (untuk Laravel 11)
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                   | Request::HEADER_X_FORWARDED_HOST
+                   | Request::HEADER_X_FORWARDED_PORT
+                   | Request::HEADER_X_FORWARDED_PROTO
+        );
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
+
+
+
+
+
+// use Illuminate\Foundation\Application;
+// use Illuminate\Foundation\Configuration\Exceptions;
+// use Illuminate\Foundation\Configuration\Middleware;
+
+// return Application::configure(basePath: dirname(__DIR__))
+//     ->withRouting(
+//         web: __DIR__.'/../routes/web.php',
+//         api: __DIR__.'/../routes/api.php',
+//         commands: __DIR__.'/../routes/console.php',
+//         health: '/up',
+//     )
+//     ->withMiddleware(function (Middleware $middleware) {
+//         $middleware->append(\App\Http\Middleware\TrustProxies::class);
+//     })
+
+//     ->withExceptions(function (Exceptions $exceptions) {
+//         //
+//     })->create(); -->
+
